32772d0c0c27fdacff60488a71692d5177471b22
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-01-28 16:18:57 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function changeButtonText(button) {
+    button.innerHTML = "Logout"; // Change the button text to "Logout"
+}
+
+function alertLike() {
+    alert('Ninja has been liked!');
+}
+
+function hide(button) {
+    button.remove();
+}
